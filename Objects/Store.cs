dc31fae8c4e3b905bf022d450266ad869ad7432f
Nathan Otto <ottoetc@gmail.com>
2016-03-04 18:22:36 -0800

    Add first test for Store class

---
+++ Objects/Store.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace ShoeStore
+{
+  public class Store
+  {
+    private int _id;
+    private string _name;
+
+    public Store(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+    public override bool Equals(System.Object otherStore)
+    {
+      if(!(otherStore is Store))
+      {
+        return false;
+      }
+      else
+      {
+        Store newStore = (Store) otherStore;
+        bool idEquality = this.GetId() == newStore.GetId();
+        bool nameEquality = this.GetName() == newStore.GetName();
+        return(idEquality && nameEquality);
+      }
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM stores;", conn);
+      cmd.ExecuteNonQuery();
+    }
+    public static List<Store> GetAll()
+    {
+      List<Store> allStores = new List<Store>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM stores;", conn);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int storeId = rdr.GetInt32(0);
+        string storeName = rdr.GetString(1);
+        Store newStore = new Store(storeName, storeId);
+        allStores.Add(newStore);
+      }
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+      return allStores;
+    }
+  }
+}
--- Tests/StoreTest.cs
+++ Tests/StoreTest.cs
@@ -1,28 +1,29 @@
-// using Xunit;
-// using System.Collections.Generic;
-// using System;
-// using System.Data;
-// using System.Data.SqlClient;
-//
-// namespace ShoeStore
-// {
-//   public class StoreTest : IDisposable
-//   {
-//     public StoreTest()
-//     {
-//       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=shoe_stores_test;Integrated Security=SSPI;";
-//     }
-//     [Fact]
-//     public void Test_StoreTableStartsEmpty()
-//     {
-//       int result = Store.GetAll().Count;
-//
-//       Assert.Equal(0, result);
-//     }
-//     public void Dispose()
-//     {
-//       // Store.DeleteAll();
-//       // Brand.DeleteAll();
-//     }
-//   }
-// }
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ShoeStore
+{
+  public class StoreTest : IDisposable
+  {
+    public StoreTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=shoe_stores_test;Integrated Security=SSPI;";
+    }
+    [Fact]
+    public void Test_StoreTableStartsEmpty()
+    {
+      int result = Store.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+
+    public void Dispose()
+    {
+      // Store.DeleteAll();
+      Brand.DeleteAll();
+    }
+  }
+}
